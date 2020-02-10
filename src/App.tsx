e792a8856890e27e4cb4f771c0d851e1904205c4
MarinaDementyeva <marishka3224@gmail.com>
2020-02-10 21:25:59 +0700

    Перенесла файлы в нужную папку

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { List as Movies } from './movies/list';
+
+import dataMovies from './mocks/movies';
+
+const App: React.FC = () => {
+    return (
+        <div className='App'>
+            {' '}
+            <Movies movieList={dataMovies} />
+        </div>
+    );
+};
+
+export default App;
